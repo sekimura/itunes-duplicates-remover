c339de706fc0547ecca102c36c47fdc566962cc2
Masayoshi Sekimura <sekimura@chacha.local>
2009-05-06 01:20:52 -0700

    fixed markdown syntax

--- itunes-dup-remover.rb
+++ itunes-dup-remover.rb
@@ -5,15 +5,17 @@
 # 
 # # Dependencies
 # 
-# [http://rubyosa.rubyforge.org/ "RubyOSA"]
+# [RubyOSA]([http://rubyosa.rubyforge.org/)
 # 
 # # How to use
 # 
-# for lazy person:
+# ## for lazy person:
+# 
 # 1. run this script "ruby itunes-dup-remover.rb" to display duplicated files
 # 2. run with '-y' option to remove them
-#
-# if your're not that lazy: (but this might be faster)
+# 
+# ## if your're not that lazy: (but this might be faster)
+# 
 # 1. create a playlist and give a unique name, "dupxxx", with in your playlists
 # 2. get duplicates by 'File'->'Show Duplicates' on your library
 # 3. add those "duplicates" into the playlist you created
